454a293295f8e13cc8186703e487134e367f07b8
sacha sardo <sacha.sardo@hotmail.fr>
2015-05-01 13:29:42 +0200

    facebook

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501094605) do
+ActiveRecord::Schema.define(version: 20150501104425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20150501094605) do
     t.string   "name"
     t.integer  "stars"
     t.string   "key_words"
-    t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "text"
     t.boolean  "good"
   end
 
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20150501094605) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "picture"
+    t.string   "name"
+    t.string   "token"
+    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
