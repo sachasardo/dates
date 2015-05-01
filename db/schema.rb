dd30ee8df90710967017286ecb9f8f9db4c6271a
sacha sardo <sacha.sardo@hotmail.fr>
2015-05-01 16:40:28 +0200

    fb

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150501104425) do
-=======
 ActiveRecord::Schema.define(version: 20150501124243) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +20,9 @@ ActiveRecord::Schema.define(version: 20150501124243) do
     t.string   "name"
     t.integer  "stars"
     t.string   "key_words"
-    t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "text"
     t.boolean  "good"
     t.integer  "user_id"
   end
